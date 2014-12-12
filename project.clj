60c60d669eb7f6e5a205ed4d15d93576f34a7e7c
Thomas Sonntag <git@sonntagsbox.de>
2014-12-12 09:48:36 +0100

    added project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject interruptible "0.1.0"
+  :description "Library for handling interrupts (ctrl-c) gracefully"
+  :url "http://github.com/tracksun/interruptible.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
